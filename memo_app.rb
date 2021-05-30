753a83f4898866466dbcffafb27e6c1a128a59f9
taka110 <bigtree.aska@gmail.com>
2021-05-31 00:47:35 +0900

    Memoクラスのメソッドの引数を削除

--- memo_app.rb
+++ memo_app.rb
@@ -12,14 +12,14 @@ helpers do # XSS対策→postとpatchでHTMLをエスケープする
 end
 
 class Memo
-  def self.load_memo(filename)
-    File.open(filename) do |file|
+  def self.load_memo
+    File.open('memos.json') do |file|
       JSON.parse(file.read)
     end
   end
 
-  def self.save_memo(filename, memo)
-    File.open(filename, 'w') do |file| # wオプションで一旦削除して丸ごと書き込み
+  def self.save_memo(memo)
+    File.open('memos.json', 'w') do |file| # wオプションで一旦削除して丸ごと書き込み
       JSON.dump(memo, file)
     end
   end
@@ -27,11 +27,11 @@ end
 
 get '/memo' do # top画面呼び出し
   if FileTest.exist?('memos.json') # memos.jsonが存在すればロードしてハッシュに入れる
-    @hash = Memo.load_memo('memos.json')
+    @hash = Memo.load_memo
     @page_title = 'top'
     erb :top
   else # memos.jsonが存在しない時はmemos.jsonを作成して強制的に新規投稿画面に行く
-    Memo.save_memo('memos.json', { 'memos': [] })
+    Memo.save_memo({ 'memos': [] })
     @page_title = 'new'
     erb :new
   end
@@ -41,9 +41,9 @@ post '/memo' do # 新規メモ保存
   @title = h(params[:title]) # メモのタイトルと内容を取得
   @body = h(params[:body])
   new_memo = { 'id': SecureRandom.uuid, 'title': @title, 'body': @body } # メモ内容をハッシュに入れる
-  @hash = Memo.load_memo('memos.json')
+  @hash = Memo.load_memo
   @hash['memos'].push(new_memo)
-  Memo.save_memo('memos.json', @hash)
+  Memo.save_memo(@hash)
   redirect '/memo'
 end
 
@@ -53,7 +53,7 @@ get '/memo/new' do # new画面表示
 end
 
 get '/memo/:id' do # show画面表示
-  @hash = Memo.load_memo('memos.json')
+  @hash = Memo.load_memo
   item = @hash['memos'].select { |memo| memo['id'] == params['id'] } # idで特定する
   @id = item[0]['id'] # 特定したメモ内容を代入
   @title = item[0]['title']
@@ -63,10 +63,10 @@ get '/memo/:id' do # show画面表示
 end
 
 delete '/memo/:id' do # メモ削除
-  @hash = Memo.load_memo('memos.json')
+  @hash = Memo.load_memo
   item = @hash['memos'].select { |memo| memo['id'] == params['id'] } # idで特定する
   @hash['memos'].delete(item[0]) # 配列から削除
-  Memo.save_memo('memos.json', @hash)
+  Memo.save_memo(@hash)
   redirect '/memo'
 end
 
@@ -74,16 +74,16 @@ patch '/memo/:id' do # メモ修正
   @title = h(params[:title]) # メモのタイトルと内容を取得
   @body = h(params[:body])
   edit_memo = { 'id': params[:id], 'title': @title, 'body': @body } # 修正したメモ内容をハッシュに入れる
-  @hash = Memo.load_memo('memos.json')
+  @hash = Memo.load_memo
   item = @hash['memos'].select { |memo| memo['id'] == params['id'] } # idで特定する
   @hash['memos'].delete(item[0]) # 配列から削除
   @hash['memos'].push(edit_memo) # 修正したメモ内容を代入
-  Memo.save_memo('memos.json', @hash)
+  Memo.save_memo(@hash)
   redirect '/memo'
 end
 
 get '/memo/:id/edit' do # edit画面表示
-  @hash = Memo.load_memo('memos.json')
+  @hash = Memo.load_memo
   item = @hash['memos'].select { |memo| memo['id'] == params['id'] } # idで特定する
   @id = item[0]['id'] # 特定したメモ内容を代入
   @title = item[0]['title']
