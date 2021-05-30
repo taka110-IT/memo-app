f0a7e41044195ee44e7567b382888d2fde80b10f
taka110 <bigtree.aska@gmail.com>
2021-05-31 01:02:01 +0900

    selectをfindに修正

--- memo_app.rb
+++ memo_app.rb
@@ -54,18 +54,18 @@ end
 
 get '/memo/:id' do # show画面表示
   @hash = Memo.load_memo
-  item = @hash['memos'].select { |memo| memo['id'] == params['id'] } # idで特定する
-  @id = item[0]['id'] # 特定したメモ内容を代入
-  @title = item[0]['title']
-  @body = item[0]['body']
+  item = @hash['memos'].find { |memo| memo['id'] == params['id'] } # idで特定する
+  @id = item['id'] # 特定したメモ内容を代入
+  @title = item['title']
+  @body = item['body']
   @page_title = 'show'
   erb :show
 end
 
 delete '/memo/:id' do # メモ削除
   @hash = Memo.load_memo
-  item = @hash['memos'].select { |memo| memo['id'] == params['id'] } # idで特定する
-  @hash['memos'].delete(item[0]) # 配列から削除
+  item = @hash['memos'].find { |memo| memo['id'] == params['id'] } # idで特定する
+  @hash['memos'].delete(item) # 配列から削除
   Memo.save_memo(@hash)
   redirect '/memo'
 end
@@ -75,8 +75,8 @@ patch '/memo/:id' do # メモ修正
   @body = h(params[:body])
   edit_memo = { 'id': params[:id], 'title': @title, 'body': @body } # 修正したメモ内容をハッシュに入れる
   @hash = Memo.load_memo
-  item = @hash['memos'].select { |memo| memo['id'] == params['id'] } # idで特定する
-  @hash['memos'].delete(item[0]) # 配列から削除
+  item = @hash['memos'].find { |memo| memo['id'] == params['id'] } # idで特定する
+  @hash['memos'].delete(item) # 配列から削除
   @hash['memos'].push(edit_memo) # 修正したメモ内容を代入
   Memo.save_memo(@hash)
   redirect '/memo'
@@ -84,10 +84,10 @@ end
 
 get '/memo/:id/edit' do # edit画面表示
   @hash = Memo.load_memo
-  item = @hash['memos'].select { |memo| memo['id'] == params['id'] } # idで特定する
-  @id = item[0]['id'] # 特定したメモ内容を代入
-  @title = item[0]['title']
-  @body = item[0]['body']
+  item = @hash['memos'].find { |memo| memo['id'] == params['id'] } # idで特定する
+  @id = item['id'] # 特定したメモ内容を代入
+  @title = item['title']
+  @body = item['body']
   @page_title = 'edit'
   erb :edit
 end
