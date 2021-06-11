44bd031a734f9b8979f0e565e76838d5ba279200
taka110-IT <onyourmark_aska@yahoo.co.jp>
2021-06-12 00:30:53 +0900

    DB版メモアプリ：rubocop通過

--- memo_app.rb
+++ memo_app.rb
@@ -10,32 +10,31 @@ helpers do # XSS対策→postとpatchでHTMLをエスケープする
   end
 end
 
-conn = PG.connect( dbname: 'dbmemo' )
+conn = PG.connect(dbname: 'dbmemo')
 
 def load_memo(conn)
   @hash = {}
   @hash['memos'] = []
-  conn.exec( "SELECT * FROM memos" ) do |result|
+  conn.exec('SELECT * FROM memos') do |result|
     result.each do |row|
-      memo = { 'id': row['id'], 'title': row['title'], 'body': row['body']}
+      memo = { 'id': row['id'], 'title': row['title'], 'body': row['body'] }
       @hash['memos'].push(memo)
     end
   end
 end
 
 get '/memo' do # top画面呼び出し
-    load_memo(conn)
-    @page_title = 'top'
-    erb :top
+  load_memo(conn)
+  @page_title = 'top'
+  erb :top
 end
 
 post '/memo' do # 新規メモ保存
   @title = h(params[:title]) # メモのタイトルと内容を取得
   @body = h(params[:body])
-  conn.prepare("find", "INSERT INTO memos (title, body) VALUES ($1,$2)")
-  post_memo = conn.exec_prepared("find", [@title, @body])
-  conn.exec('DEALLOCATE find')
-  post_memo
+  conn.prepare('top', 'INSERT INTO memos (title, body) VALUES ($1,$2)')
+  conn.exec_prepared('top', [@title, @body])
+  conn.exec('DEALLOCATE top')
   redirect '/memo'
 end
 
@@ -55,20 +54,18 @@ get '/memo/:id' do # show画面表示
 end
 
 delete '/memo/:id' do # メモ削除
-  conn.prepare("find", "DELETE FROM memos WHERE id=$1")
-  delete_memo = conn.exec_prepared("find", [params['id']])
-  conn.exec('DEALLOCATE find')
-  delete_memo
+  conn.prepare('delete', 'DELETE FROM memos WHERE id=$1')
+  conn.exec_prepared('delete', [params['id']])
+  conn.exec('DEALLOCATE delete')
   redirect '/memo'
 end
 
 patch '/memo/:id' do # メモ修正
   @title = h(params[:title]) # メモのタイトルと内容を取得
   @body = h(params[:body])
-  conn.prepare("find", "UPDATE memos SET title=$2, body=$3 WHERE id=$1")
-  update_memo = conn.exec_prepared("find", [params['id'], @title, @body])
-  conn.exec('DEALLOCATE find')
-  update_memo
+  conn.prepare('update', 'UPDATE memos SET title=$2, body=$3 WHERE id=$1')
+  conn.exec_prepared('update', [params['id'], @title, @body])
+  conn.exec('DEALLOCATE update')
   redirect '/memo'
 end
 
